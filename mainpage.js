e898cf43e66acb1739cc381065e2815add367b0f
lorry2333 <contata23@gmail.com>
2021-11-28 03:56:30 -0500

    basic layout done

---
+++ mainpage.js
@@ -0,0 +1,11 @@
+/* Open */
+function openNav() {
+    document.getElementById("recipe").style.height = "100%";
+    console.log('open')
+  }
+  
+  /* Close */
+  function closeNav() {
+    document.getElementById("recipe").style.height = "0%";
+    console.log('close')
+  }
